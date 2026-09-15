e931f5324f1697020ebcb24707256ccb553cf400
ammwhat <akj172001@gmail.com>
2026-09-16 00:50:35 +0530

    Fix header key casing for User-Agent

--- Phase 4 web&Api's/DAY 40 ECOM DOM PARSING/Ecom_pricescrapper.py
+++ Phase 4 web&Api's/DAY 40 ECOM DOM PARSING/Ecom_pricescrapper.py
@@ -11,7 +11,7 @@ MY_PASSWORD = os.getenv("MY_PASSWORD")
 
 
 my_headers = {
-    "User-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/153.0.0.0 Safari/537.36",
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/153.0.0.0 Safari/537.36",
     "Accept-language" : "en-US,en;q=0.9"
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Connection": "keep-alive",
